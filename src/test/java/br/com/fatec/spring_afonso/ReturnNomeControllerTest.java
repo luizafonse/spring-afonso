ab4a502ef4d072102bbd3938e98a5aa9aad494cd
Afonso Luiz <sonicbrasilgames@gmail.com>
2025-11-03 17:40:44 -0300

    @luizafonse

--- src/test/java/br/com/fatec/spring_afonso/ReturnNomeControllerTest.java
+++ src/test/java/br/com/fatec/spring_afonso/ReturnNomeControllerTest.java
@@ -10,12 +10,14 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import br.com.fatec.spring_afonso.controller.ReturnNomeController;
 
 @WebMvcTest({ReturnNomeController.class})
-public class RetunrNomeControllerTest {
+public class ReturnNomeControllerTest {
    @Autowired
    private MockMvc mockMvc;
-   @Test
-   public void ReturnNomeControllerTest() {
+
+   public ReturnNomeControllerTest() {
    }
+
+   @Test
    public void testMeuNome() throws Exception {
       this.mockMvc.perform(MockMvcRequestBuilders.get("/meu-nome", new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().string("Afonso Luiz Soares Batista"));
    }
